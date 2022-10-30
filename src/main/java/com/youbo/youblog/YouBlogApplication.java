5a4c5d19a7c8d0b8439d8558f8edbb1606cc3f94
youbo <xiaobo.you@outlook.com>
2022-10-30 21:01:44 +0800

    youBlog初始化项目

---
+++ src/main/java/com/youbo/youblog/YouBlogApplication.java
@@ -0,0 +1,13 @@
+package com.youbo.youblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YouBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YouBlogApplication.class, args);
+    }
+
+}
